f8d2105b5f3449e9545710363ce7ffe5a66d3e52
wnjstudio <wnjstudio@outlook.com>
2018-11-11 19:48:53 +0100

    Applying Prettier Format

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const adminCredentials = { wnjstudio: 'clqe41799' }
+const adminCredentials = { wnjstudio: "clqe41799" }
 
 const path = require("path")
 const express = require("express")
 
 const app = express()
 const server = require("http").createServer(app)
-const io = require('socket.io').listen(server)
+const io = require("socket.io").listen(server)
 
 const spawn = require("child_process").spawn
 
@@ -15,65 +15,64 @@ let py
 let clientCount = 0
 
 const parser = data => {
-    let obj = {}
-    let p = /(.+?)@=(.*?)\//g
-    let r
-    while (r = p.exec(data)) {
-        obj[r[1]] = r[2]
-    }
-    return obj
+  let obj = {}
+  let p = /(.+?)@=(.*?)\//g
+  let r
+  while ((r = p.exec(data))) {
+    obj[r[1]] = r[2]
+  }
+  return obj
 }
 
 const reject = {
-    keeplive: true,
-    qausrespond: true,
-    rnewbc: true,
-    anbc: true,
-    spbc: true,
-    loginres: true,
-    pingreq: true,
-    gbroadcast: true
-
+  keeplive: true,
+  qausrespond: true,
+  rnewbc: true,
+  anbc: true,
+  spbc: true,
+  loginres: true,
+  pingreq: true,
+  gbroadcast: true,
 }
 
 app.use(express.static(path.resolve(__dirname, "client/build")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
 })
 
-io.on('connection', client => {
+io.on("connection", client => {
+  clientCount++
+  console.log(`User Count: ${clientCount}`)
 
-    clientCount++
+  if (!py && clientCount > 0) {
+    console.log("First User In - Starting Crawler")
+    py = spawn("python3", ["crawler.py", roomID], { detached: true })
+    py.stdout.on("data", data => {
+      data = parser(data.toString("utf8"))
+      if (!reject[data.type]) io.emit("data", data)
+    })
+  }
+
+  client.on("disconnecting", res => {
+    clientCount--
     console.log(`User Count: ${clientCount}`)
-
-    if (!py && clientCount > 0) {
-        console.log('First User In - Starting Crawler')
-        py = spawn("python3", ['crawler.py', roomID], { detached: true })
-        py.stdout.on('data', data => {
-            data = parser(data.toString('utf8'))
-            if (!reject[data.type])
-                io.emit('data', data)
-        })
+    if (clientCount <= 0) {
+      console.log("Last User Out - Killing Crawler")
+      process.kill(-py.pid)
+      py = undefined
     }
+  })
 
-    client.on('disconnecting', res => {
-        clientCount--
-        console.log(`User Count: ${clientCount}`)
-        if (clientCount <= 0) {
-            console.log('Last User Out - Killing Crawler')
-            process.kill(-py.pid)
-            py = undefined
-        }
-    })
+  client.emit("roomid", roomID)
 
-    client.emit('roomid', roomID)
-
-    client.on('login', ts => {
-        console.log(new Date(ts).toLocaleString())
-    })
+  client.on("login", ts => {
+    console.log(new Date(ts).toLocaleString())
+  })
 })
 
-
-
-server.listen(process.env.PORT, process.env.IP || "0.0.0.0", () => console.log(`Server ${server.address().address} on port ${server.address().port}`))
+server.listen(process.env.PORT, process.env.IP || "0.0.0.0", () =>
+  console.log(
+    `Server ${server.address().address} on port ${server.address().port}`
+  )
+)
